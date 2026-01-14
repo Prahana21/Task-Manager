313f50c646e7fd07a4c1813b03c1f1673d4e0047
Prahana Mondal <mondalprahana2@gmail.com>
2026-01-14 10:04:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 app.use(express.static('./public'))
 app.use(express.json())
+app.use('/api/v1/tasks', tasks)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 //routes
 
 
-app.use('/api/v1/tasks', tasks)
+
 const port = process.env.PORT || 3000
 //Database logic is separated into its own module (connectDB)
 
